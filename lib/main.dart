f893bce94eff6edee527d0e1f423ef2aeab320ea
Jose Guerrero <jose.guerrero10@yahoo.com>
2020-09-05 18:30:11 -0700

    simple app

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(Heet());
+
+class Heet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Bienvenue a Heet!",
+        home: Scaffold(
+            appBar: AppBar(title: Text("Bienvenue")),
+            body: Center(child: RandomWords())));
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
