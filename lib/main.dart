ff524ce645b588bfcf445b1c9a0aaa5c05ea69c2
Jose Guerrero <jose.guerrero10@yahoo.com>
2020-09-08 08:29:37 -0700

    added animation to card

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,170 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter/physics.dart';
 
-void main() => runApp(Heet());
+void main() => runApp(PSETester());
 
-class Heet extends StatelessWidget {
+class PSETester extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Bienvenue a Heet!",
+        title: "Virtual Study Groups",
         home: Scaffold(
-            appBar: AppBar(title: Text("Bienvenue")),
-            body: Center(child: RandomWords())));
+            appBar: AppBar(title: Text("Virtual Study Groups")),
+            body: CardWidget(
+              child: FlutterLogo(size: 128),
+            )));
   }
 }
 
-class RandomWords extends StatefulWidget {
+
+/*
+class ChatScreen extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _ChatScreenState createState() => _ChatScreenState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    InputDecoration.collapsed(hintText: "Send a message "),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconButton(
+                    icon: const Icon(Icons.send),
+                    onPressed: () => _handleSubmitted(_textController.text)))
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return _buildTextComposer();
   }
 }
+
+class CardWidget extends StatefulWidget {
+  final Widget child;
+  CardWidget({this.child});
+
+  @override
+  _CardState createState() => _CardState();
+}
+
+class _CardState extends State<CardWidget> with SingleTickerProviderStateMixin {
+  // Setup animation controller to animate alignment
+  // of card when dragged away from the center
+  AnimationController _controller;
+  Alignment _dragAlignment = Alignment.center;
+  Animation<Alignment> _animation;
+
+  void _runAnimation(Offset pixelsPerSecond, Size size) {
+    _animation = _controller
+        .drive(AlignmentTween(begin: _dragAlignment, end: Alignment.center));
+
+    // Calculate the velocity
+    // final doesnt change but is assigned at runtime
+    final unitsPerSecondX = pixelsPerSecond.dx / size.width;
+    final unitsPerSecondY = pixelsPerSecond.dy / size.height;
+    final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
+    final unitVelocity = unitsPerSecond.distance;
+
+    const spring = SpringDescription(mass: 30, stiffness: 1, damping: 1);
+    final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
+    _controller.animateWith(simulation);
+  }
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    super.initState();
+    _controller
+        .addListener(() => setState(() => {_dragAlignment = _animation.value}));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onPanDown: (details) => {_controller.stop()},
+      onPanUpdate: (details) => {
+        setState(() {
+          _dragAlignment += Alignment(details.delta.dx / (size.width / 2),
+              details.delta.dy / (size.height / 2));
+        })
+      },
+      onPanEnd: (details) =>
+          {_runAnimation(details.velocity.pixelsPerSecond, size)},
+      child: Align(
+        alignment: _dragAlignment,
+        child: Card(child: widget.child),
+      ),
+    );
+  }
+}
+*/
+/*
+class Align extends StatelessWidget {
+  const Align({
+    Key key,
+    @required this.widget,
+  }) : super(key: key);
+
+  final CardWidget widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: Card(
+        child: widget.child,
+      )
+    );
+  }
+}*/
+//Flare , adaptive setting for widgets, get_it, provider
+// widgets- wrap direction axis.vertical, safearea, RichText add TextSpan with style and children
+// with other TextSpan with style, wrap image with ClipRRect give container clips
+// border radius, media query of context to get size brightness , futurebuilder takes a future and
+// snapshot in builder can return progress indicator or
+// flexible - give first widget as flex 1 and takes all the flexes remove heights
+// sizedbox - give a perfect width and height can give double.infinity expanded
+// align property - aligment.bottomright
+//animationcontroller transform and stack
+// start with sketch - google
+// play around colors and features
+// design parts in rive using flat shapes
+// rigging - think before start rigging
+// front image moves a little more than back parallax - 3d effect
+// using flareactor
+// use provider - disposables provider has dispose callback
+// use changenotifierprovider
+// use multiprovider - listen to parrts
+//
+
+// flutter run -d chrome --web-port=8080 --web-hostname=127.0.0.1
